ddf04bb3d9802dac7419a756327165e71be3149f
kritika <kbhat3@asu.edu>
2023-07-19 00:33:28 +0530

    removed draw alert, added round winner

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const rockBtn = document.getElementById('rockBtn')
-const paperBtn = document.getElementById('paperBtn')
-const scissorsBtn = document.getElementById('scissorsBtn')
-
-const userTally = document.getElementById('userTally')
 const computerTally = document.getElementById('computerTally')
 const finalTally = document.getElementById('finalTally')
+const paperBtn = document.getElementById('paperBtn')
+const rockBtn = document.getElementById('rockBtn')
+const roundWon = document.getElementById('roundWon')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const userTally = document.getElementById('userTally')
 
-let scoreUser = 0
-let scoreComputer = 0
-let finalWinner = ""
 let computerChoice = ""
-let numberOfPlays = 0
+let finalWinner = ""
+let scoreComputer = 0
+let scoreUser = 0
 
 function randomChoice () {
     let choices = ["Rock", "Paper", "Scissors"]
@@ -18,90 +17,99 @@ function randomChoice () {
 }
 
 function playRound (userChoice, computerChoice) {
-    let result
+    let roundWinner, winningChoice
     if (String(userChoice).toUpperCase() === "ROCK") {
         switch(String(computerChoice).toUpperCase()) {
             case "ROCK": 
-                result = "Draw!"
+                roundWinner = "Draw!"
+                winningChoice = "Draw"
                 break; 
             case "PAPER": 
-                result = "You Lose!"
+                roundWinner = "You Lose!"
+                winningChoice = "Paper Won"
                 break; 
             case "SCISSORS": 
-                result = "You Win!"
+                roundWinner = "You Win!"
+                winningChoice = "Rock Won"
                 break; 
-            default: 
-                result = "Invalid input!"
-                break;
         }
     } else if (String(userChoice).toUpperCase() === "PAPER") {
         switch(String(computerChoice).toUpperCase()) {
             case "PAPER": 
-                result = "Draw!"
+                roundWinner = "Draw!"
+                winningChoice = "Draw"
                 break; 
             case "ROCK": 
-                result = "You Win!"
+                roundWinner = "You Win!"
+                winningChoice = "Paper Won"
                 break; 
             case "SCISSORS": 
-                result = "You Lose!"
+                roundWinner = "You Lose!"
+                winningChoice = "Scissors Won"
                 break; 
-            default: 
-                result = "Invalid input!"
-                break;
         }
     } else {
         switch(String(computerChoice).toUpperCase()) {
             case "SCISSORS": 
-                result = "Draw!"
+                roundWinner = "Draw!"
+                winningChoice = "Draw"
                 break; 
             case "PAPER": 
-                result = "You Win!"
+                roundWinner = "You Win!"
+                winningChoice = "Scissors Won"
                 break; 
             case "ROCK": 
-                result = "You Lose!"
+                roundWinner = "Rock Won"
                 break; 
-            default: 
-                result = "Invalid input!"
-                break;
         }
     }
-    return result
+    return { roundWinner, winningChoice }
 }
 
-rockBtn.addEventListener('click', () => startPlayOnClick('rock'))
 paperBtn.addEventListener('click', () => startPlayOnClick('paper'))
+rockBtn.addEventListener('click', () => startPlayOnClick('rock'))
 scissorsBtn.addEventListener('click', () => startPlayOnClick('scissors'))
 
 function startPlayOnClick (userChoice) {
     computerChoice = randomChoice()
-    let thisWinner = playRound(userChoice, computerChoice)
-    updateScoreTally(thisWinner)
+    let { roundWinner, winningChoice} = playRound(userChoice, computerChoice)
+    updateScoreTally(roundWinner)
+    showRoundWinner(winningChoice)
 }
 
-function updateScoreTally (thisWinner) {
-    numberOfPlays++;
-    if (thisWinner === "You Lose!") {
-        ++scoreComputer
-        computerTally.textContent = `Computer: ${scoreComputer}`
-    } else if(thisWinner === "You Win!") {
-        ++scoreUser
-        userTally.textContent = `Player: ${scoreUser}`
-    } else if (thisWinner === "Draw!") {
-        alert("This turn was a draw.")
-    }
-    if (numberOfPlays === 5) declareWinner()
+function showRoundWinner(winningChoice) {
+    if (winningChoice === undefined || winningChoice === null) return
+    roundWon.style.border = "1px solid black"
+    roundWon.style.borderRadius = "8px"
+    roundWon.style.display = "flex"
+    roundWon.style.padding = "10px"
+    roundWon.textContent = winningChoice
 }
 
-function declareWinner() {
-    if (scoreComputer > scoreUser) finalWinner= "Computer Wins!"
-    else if (scoreComputer < scoreUser) finalWinner= "You win!"
-    else finalWinner = "Draw!"
- 
-    finalTally.style.display = "flex"
-    finalTally.style.borderRadius = "8px"
+function showFinalTally(finalWinner) {
     finalTally.style.border = "1px solid black"
+    finalTally.style.borderRadius = "8px"
+    finalTally.style.display = "flex"
     finalTally.style.padding = "10px"
     finalTally.textContent = finalWinner
+}
 
+function updateScoreTally (roundWinner) {
+    if (roundWinner === "You Lose!") {
+        ++scoreComputer
+        computerTally.textContent = `Computer: ${scoreComputer}`
+    } else if(roundWinner === "You Win!") {
+        ++scoreUser
+        userTally.textContent = `Player: ${scoreUser}`
+    }
+    if (scoreComputer === 5 || scoreUser === 5) {
+        declareWinner(scoreComputer, scoreUser)
+    }
+}
+
+function declareWinner(scoreComputer, scoreUser) {
+    if (scoreComputer > scoreUser) finalWinner = "Computer Wins!"
+    else finalWinner= "You win!"
+    showFinalTally(finalWinner)
     alert("Game has ended. Refresh page to try again!")
 }
