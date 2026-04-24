92bb638568703cf277b698d5435a0fde53f4665e
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-04-24 02:53:51 -0400

    feat: added rock-paper-scissors logic

--- script.js
+++ script.js
@@ -4,16 +4,45 @@ let computerScore = 0
 function getComputerChoice () {
     let randomNumber = Math.random() * 10
     
-    if(randomNumber <=3) return "Rock"
+    if(randomNumber <=3) return "ROCK"
         else if (randomNumber > 3 && randomNumber <= 6.5)
-            return "Paper"
-        else return "Scissors"
+            return "PAPER"
+        else return "SCISSORS"
 }
-
-console.log(getComputerChoice())
 
 function getHumanChoice () {
     return prompt("Pick one!")
 }
 
-console.log(getHumanChoice())
+let computerChoice = getComputerChoice()
+let humanChoice = getHumanChoice().toUpperCase()
+
+
+function playRound (humanChoice, computerChoice) {
+    console.log("You Pick: " + humanChoice)
+    console.log("Computer Picks: " + computerChoice)
+    let didPCWin = true // assume PC wins
+
+    if (humanChoice === computerChoice) {
+        console.log("Draw!")
+        return
+    }
+
+    if (humanChoice === "ROCK" && computerChoice === "SCISSORS") 
+        didPCWin = false
+    if (humanChoice === "PAPER" && computerChoice === "ROCK")
+        didPCWin = false
+    if (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+        didPCWin = false
+
+    if (didPCWin) {
+        console.log("PC wins!")
+        ++computerScore
+    } else {
+        console.log("You win!")
+        ++humanScore
+    }
+}
+
+playRound(humanChoice, computerChoice)
+console.log("Current scores:\nHuman: " + humanScore + " PC: " + computerScore)
