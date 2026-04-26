49776aee0b4ef1587ac75864a2b408777635d536
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-04-26 00:14:12 -0400

    feat: added UI to the game
    BREAKING CHANGE: play by selecting buttons instead of prompt

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+const gameSection = document.querySelector('.gameSection')
 let humanScore = 0
 let computerScore = 0
+let humanChoice = ""
+let numberOfRounds = 0
 
 function getComputerChoice () {
     let randomNumber = Math.random() * 10
-    
     if(randomNumber <=3) return "ROCK"
         else if (randomNumber > 3 && randomNumber <= 6.5)
             return "PAPER"
         else return "SCISSORS"
 }
 
-function getHumanChoice () {
-    return prompt("Pick one!")
-}
+gameSection.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        ++numberOfRounds
+        humanChoice = e.target.textContent.toUpperCase()
+        playRound(humanChoice)
+        console.log("Current scores:\nHuman: " + humanScore + " PC: " + computerScore)
+    }
 
-function playRound () {
-    let humanChoice = getHumanChoice().toUpperCase()
+    if (numberOfRounds === 5)
+        pickWinner()
+})
+
+function playRound (humanChoice) {
     let computerChoice = getComputerChoice()
 
-    console.log("You Pick: " + humanChoice)
-    console.log("Computer Picks: " + computerChoice)
+    // Need to show the computer choice and number of rounds on a line
+    // console.log("Computer Picks: " + computerChoice)
+
     let didPCWin = true // assume PC wins
 
     if (humanChoice === computerChoice) return
@@ -35,15 +45,20 @@ function playRound () {
     else ++humanScore
 }
 
-function playGame () {
-    for (let i = 0; i < 5; i ++) {
-        playRound()
-        console.log("Current scores:\nHuman: " + humanScore + " PC: " + computerScore)
-    }
-
+function pickWinner () {
     (humanScore > computerScore) ? console.log("You Won!")
-        : (computerScore > humanScore) ? console.log("PC Won!")
-        : console.log("Draw!")
+    : (computerScore > humanScore) ? console.log("PC Won!")
+    : console.log("Draw!")
 }
 
-playGame()
+/**
+ * After each round, use altert to tell who won
+ * Update score in the "current scores" section
+ * To show final scores, add a new div
+ * 
+ * Add some type of effects to show which button was selected by the user
+ * 
+ * add at the end, optionally
+ *             Trivia: Did you know that the popular band, BTS is the official
+            brand ambassadors of Rock, Paper, Scissors!
+ */
